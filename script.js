5e0f066b61406b05ca1f70638341e328ecf5dc1b
Rafael Carvalho <rafaelhenriquecarvalho@outlook.com>
2022-02-28 02:15:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function mascara(numero){ 
+    
+    if(numero.value.length == 0)
+        numero.value = '(' + numero.value;
+
+    if(numero.value.length == 3)
+        numero.value = numero.value + ') ';
+
+    if(numero.value.length == 10)
+        numero.value = numero.value + '-';
+
+}
+
+function chamar() {
+
+    let numero = document.querySelector('#numero').value
+    let cel = document.querySelector('#cel')
+
+    numero = numero.replace(/\D/g, '');
+
+    if(numero.length < 11) {
+
+            window.location.reload();
+
+    }
+
+    else{
+
+    cel.setAttribute('href', 'https://api.whatsapp.com/send?phone=' + numero)
+
+    }
+
+
+}
+
+
+
